b82303946548b18e309aedb01285df3f6d177a8c
Muhammad Hamza <mh408800@gmail.com>
2024-01-23 14:22:59 +0500

    adding category controller

--- controller/categoryController/categoryController.js
+++ controller/categoryController/categoryController.js
@@ -1,5 +1,34 @@
-const addingCategory = (req, res) => {
-  res.end("hello from category controller");
+const {
+  categoryModel,
+  categoryValidationSchema,
+} = require("../../models/categoryModel/categoryModel");
+
+const addingCategory = async (req, res) => {
+  // res.end("hello from category controller");
+  const data = req.body;
+  const validateResult = categoryValidationSchema.validate(data);
+  if (validateResult.error) {
+    return res.status(400).json({
+      message: "validation error",
+      error: validateResult.error.details,
+    });
+  }
+  try {
+    const newCategory = await categoryModel.create(data);
+    console.log("new category added:", newCategory);
+    return res
+      .status(201)
+      .json({
+        statusCode: 201,
+        message: "category added successfully!",
+        data: newCategory,
+      });
+  } catch (error) {
+    console.log("internal server error- adding category conytroller", error);
+    return res
+      .status(500)
+      .json({ message: "internal server error", error: error });
+  }
 };
 
 module.exports = {
